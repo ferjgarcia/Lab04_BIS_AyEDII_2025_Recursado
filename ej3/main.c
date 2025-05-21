bbd525ba0e78a0f2526baa1792a658b4208ac75c
FernandoJGarcia <fernandojaviergarcia04@gmail.com>
2025-05-21 10:26:35 -0300

    Termino el ejercicio 3

--- ej3/main.c
+++ ej3/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LINKED_LIST_LENGTH 3
+
 typedef struct Node {
     int data;
     struct Node* next;
@@ -12,9 +14,25 @@ typedef Node *List;
  * @brief Construye y devuelve una lista de ejemplo de 3 elementos
  */
 List setup_example() {
-    //
-    // COMPLETAR ACÁ!
-    //
+    List my_list = NULL;
+    Node *a_node = NULL;
+    unsigned int i = LINKED_LIST_LENGTH; 
+
+    my_list = malloc(sizeof(struct Node));
+    a_node = my_list;
+    i = i-1; 
+
+    while (i > 0) {
+        a_node->data = i*10;
+        a_node->next = malloc(sizeof(struct Node));
+        a_node = a_node->next;
+        i--;
+    }
+    
+    a_node->data = 0;
+    a_node->next = NULL;
+
+    return my_list;
 }
 
 /**
@@ -23,9 +41,16 @@ List setup_example() {
  * Precondicion: la lista xs no debe ser vacía
  */
 void append_example(List xs) {
-    //
-    // COMPLETAR ACÁ!
-    //
+    Node *a_node = NULL;
+    a_node = xs;
+
+    while (a_node->next != NULL) {
+        a_node = a_node->next;
+    }
+
+    a_node->next = malloc(sizeof(struct Node));
+    a_node->next->data = 88;
+    a_node->next->next = NULL;   
 }
 
 void show_list(List xs) {
