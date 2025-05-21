be7f35a7334f4d7b22314fb9c339e95a5d8c5777
FernandoJGarcia <fernandojaviergarcia04@gmail.com>
2025-05-21 09:51:45 -0300

    Agrego el comprimido y el enunciado

--- ej2/main.c
+++ ej2/main.c
@@ -12,9 +12,25 @@ typedef Node *List;
  * @brief Construye y devuelve una lista de ejemplo de 3 elementos
  */
 List setup_example() {
-    //
-    // COMPLETAR ACÁ!
-    //
+    List my_list;
+    unsigned int i = 3;
+    Node a_node; 
+
+    my_list = malloc(i * sizeof(struct Node));
+
+    i = i-1; 
+
+    while (i > 0) {
+        a_node.data = i*10;
+        a_node.next = malloc(sizeof(struct Node));
+        a_node.next = a_node.next;
+        i--;
+    }
+    
+    a_node.data = 0;
+    a_node.next = NULL;
+
+    return my_list;
 }
 
 void show_list(List xs) {
